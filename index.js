16f84b71c4489d9201342e04af5f915818b63a78
ManuelSeisdedos <seisdedosmanuel2@gmail.com>
2023-04-28 18:41:50 -0300

    Algunos puntos logicos de Tateti

--- index.js
+++ index.js
@@ -1,32 +1,63 @@
 const botonesTateti = document.querySelectorAll(".ubicacion");
-
-const fichas = [
-    "x", "o", "x", "o", "x", "o", "x","o","x"
-]
+let termino = 0;
+const fichas = ["X", "O", "X", "O", "X", "O", "X","O","X"]
 
 botonesTateti.forEach(boton => {
     let jugador = document.getElementById(boton.id)
     boton.addEventListener("click", () => {
+        if(typeof tablero[jugador.id - 1] !== "number" || termino === 1 ) return
         jugador.innerHTML = fichas[0]
         ponerFicha(jugador.id)
-        
+        if (terminoElJuego() !== undefined) {
+            termino = 1
+            return setTimeout(() => alert(terminoElJuego()), 100)
+        }
     })
 })
 
 const tablero = [
-cuadrado1="", 
-cuadrado2="",
-cuadrado3="",
-cuadrado4="",
-cuadrado5="",
-cuadrado6="",
-cuadrado7="",
-cuadrado8="",
-cuadrado9="",
+cuadrado1=1, 
+cuadrado2=2,
+cuadrado3=3,
+cuadrado4=4,
+cuadrado5=5,
+cuadrado6=6,
+cuadrado7=7,
+cuadrado8=8,
+cuadrado9=9,
 ]
 
 
 function ponerFicha(id) {
-    let cuadrado = tablero[id]
+    id = id - 1
     tablero[id] = fichas.shift()
+}
+
+function terminoElJuego() {
+    
+    if (tablero[0] === tablero[1] && tablero[1] === tablero[2]) {
+        return `Ganó el jugador ${tablero[0]}`
+    }
+    if (tablero[3] === tablero[4] && tablero[4] === tablero[5]) {
+        return `Ganó el jugador ${tablero[3]}`
+    }
+    if (tablero[6] === tablero[7] && tablero[7] === tablero[8]) {
+        return `Ganó el jugador ${tablero[6]}`
+    }
+    if (tablero[0] === tablero[3] && tablero[3] === tablero[6]) {
+        return `Ganó el jugador ${tablero[0]}`
+    }
+    if (tablero[1] === tablero[4] && tablero[4] === tablero[7]) {
+        return `Ganó el jugador ${tablero[1]}`
+    }
+    if (tablero[2] === tablero[5] && tablero[5] === tablero[8]) {
+        return `Ganó el jugador ${tablero[2]}`
+    }
+    if (tablero[0] === tablero[4] && tablero[4] === tablero[8]) {
+        return `Ganó el jugador ${tablero[0]}`
+    }
+    if (tablero[6] === tablero[4] && tablero[4] === tablero[2]) {
+        return `Ganó el jugador --> ${tablero[6]}`
+    }
+    if (fichas.length === 0) return "El juego terminó en empate"
 }
