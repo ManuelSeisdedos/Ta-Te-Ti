8981e249e8729c56bcfd3c8a70756a1bf5dab346
ManuelSeisdedos <seisdedosmanuel2@gmail.com>
2023-08-02 19:22:14 -0300

    Chat

--- server/server.js
+++ server/server.js
@@ -5,29 +5,23 @@ const http = require('http')
 
 const port = process.env.PORT || 3000;
 const publicPath = path.resolve(__dirname, '../public');
-
 const app = express();
 let server = http.createServer(app)
 
 app.use(express.static(publicPath));
 
+server.listen(port, (err) => {
+    if (err) throw new Error(err);
+    console.log(`Servidor corriendo en puerto ${ port }`);
+});
+
 // IO = comunicacion directa con el servidor.
 let io = socketIO(server)
 
-
-io.on("connection", (socket) => {
+// websockets
+io.on('connection', (socket) => {
     console.log(socket.id)
     socket.on("hello", (arg) => {
       console.log(arg); // world
     });
 });
-
-
-server.listen(port, (err) => {
-
-    if (err) throw new Error(err);
-
-    console.log(`Servidor corriendo en puerto ${ port }`);
-
-});
-
